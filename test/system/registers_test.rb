6b1fdb48763a11b560c60c730c875449b9d8cf32
Robi <robi.maliqi@hotmail.co.uk>
2024-01-19 20:57:51 +0000

    Created a failing system test, in the process of making it pass by generating home page with the registration features

---
+++ test/system/registers_test.rb
@@ -0,0 +1,18 @@
+require "application_system_test_case"
+
+class RegistersTest < ApplicationSystemTestCase
+  test "registering for an account" do
+    visit '/'
+
+    click_on 'Sign up'
+
+    fill_in 'First name', with: 'Chris'
+    fill_in 'Last name', with: 'Smith'
+    fill_in 'Email', with: 'chris@example.com'
+    fill_in 'Password', with: 'password123'
+
+    click_on 'Register'
+  
+    assert_selector "h1", text: "My classes"
+  end
+end
