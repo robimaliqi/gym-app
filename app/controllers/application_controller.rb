e2017d8178190d01ae6a3892516c7e0cb545a54a
Robi <robi.maliqi@hotmail.co.uk>
2024-02-09 19:44:49 +0000

    Update routes with new scaffold to gym_glasses_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
-    sessions_path
+    gym_classes_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :sessions
+  resources :gym_classes
   devise_for :users
   root 'home#index'
 end
