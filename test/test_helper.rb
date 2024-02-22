3f78311e00ee78ec4299d94c34f6d599fe4fcf02
Robi <robi.maliqi@hotmail.co.uk>
2024-02-22 20:29:00 +0000

    Add a register method in the test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,14 +2,16 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
-module ActiveSupport
-  class TestCase
-    # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
-
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
-
-    # Add more helper methods to be used by all tests here...
+module TestHelpers
+  def register
+    visit '/'
+    click_on 'Sign up'
+    fill_in 'First name', with: 'Chris'
+    fill_in 'Last name', with: 'Smith'
+    fill_in 'Email', with: 'chris@example.com'
+    fill_in 'Password', with: 'password123'
+    fill_in 'Password confirmation', with: 'password123'
+    click_on 'Sign up'
+    assert_selector "h1", text: "My Classes"
   end
 end
