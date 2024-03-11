fd6f28622b1bf194bfc2190ca3a0d359e781e06a
Robi <robi.maliqi@hotmail.co.uk>
2024-03-11 20:22:10 +0000

    Update controller with new create method to allow logged in users to add a gym class to their bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,4 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: %i[ show edit update destroy ]
 
   # GET /bookings or /bookings.json
   def index
@@ -19,42 +18,24 @@ class BookingsController < ApplicationController
   def edit
   end
 
-  # POST /bookings or /bookings.json
+  # POST
   def create
-    @booking = Booking.new(booking_params)
-
-    respond_to do |format|
-      if @booking.save
-        format.html { redirect_to booking_url(@booking), notice: "Booking was successfully created." }
-        format.json { render :show, status: :created, location: @booking }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @booking.errors, status: :unprocessable_entity }
-      end
+    @booking = current_user.bookings.build
+    @booking.gym_class_id = params[:gym_class_id]
+  
+    if @booking.save
+      redirect_to bookings_url
+    else
+      render :index
     end
   end
 
-  # PATCH/PUT /bookings/1 or /bookings/1.json
+  # PATCH/PUT
   def update
-    respond_to do |format|
-      if @booking.update(booking_params)
-        format.html { redirect_to booking_url(@booking), notice: "Booking was successfully updated." }
-        format.json { render :show, status: :ok, location: @booking }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @booking.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
-  # DELETE /bookings/1 or /bookings/1.json
+  # DELETE
   def destroy
-    @booking.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to bookings_url, notice: "Booking was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
 
   private
