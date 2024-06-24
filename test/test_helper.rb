1b37a9f4923f23996aaca89e85ab5c020bb283a7
Robi <robi.maliqi@hotmail.co.uk>
2024-06-24 18:57:00 +0100

    Added a log in method to the test helper DRY

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,6 +12,15 @@ module TestHelpers
     fill_in 'Password', with: 'password123'
     fill_in 'Password confirmation', with: 'password123'
     click_on 'Sign up'
+  end
+
+  def login
+    User.create(email: 'chris@example.com', password: 'password123')
+    visit '/'
+    click_on 'Log in'
+    fill_in 'Email', with: 'chris@example.com'
+    fill_in 'Password', with: 'password123'
+    click_on 'Log in'
     assert_selector "h1", text: "My Classes"
   end
 end
