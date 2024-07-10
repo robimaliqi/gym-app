11f12edc435024a1aec237d43af5e0bc471ac217
Robi <robi.maliqi@hotmail.co.uk>
2024-07-10 19:52:18 +0100

    Added subscription table to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_23_203121) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_26_184702) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,9 +31,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_23_203121) do
     t.integer "price"
   end
 
-  create_table "sessions", force: :cascade do |t|
+  create_table "subscriptions", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_subscriptions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,4 +52,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_23_203121) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "subscriptions", "users"
 end
