ba7fc0f964a26078cfa7e285ae428ddd6da894bc
Robi <robi.maliqi@hotmail.co.uk>
2024-07-21 19:47:00 +0100

    Update routes so that pay as you go route is as pay as you go

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get '/payment_method_selection', to: 'subscriptions#payment_method_selection'
-  post '/pay_as_you_go', to: 'subscriptions#pay_as_you_go'
+  post '/pay_as_you_go', to: 'pay_as_you_go#pay_as_you_go'
   post '/pay_monthly', to: 'subscriptions#pay_monthly'
   get '/checkout', to: 'subscriptions#stripe_checkout'
   get '/checkout/success', to: 'subscriptions#checkout_success', as: 'checkout_success'
   get '/checkout/cancel', to: 'subscriptions#checkout_cancel', as: 'checkout_cancel'
+
 end
