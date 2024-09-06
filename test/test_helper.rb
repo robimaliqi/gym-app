93ba037c756cd25482e9bb5977f9a3fc02b6d214
Robi <robi.maliqi@hotmail.co.uk>
2024-09-06 20:50:51 +0100

    Added relevant code to configure chrome for github actions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -25,3 +25,28 @@ module TestHelpers
     assert_selector "h1", text: "Home#index"
   end
 end
+
+is_ci = ENV.fetch('IS_CI', false)
+
+Capybara.register_driver :headless_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(
+    args: %w[headless no-sandbox disable-gpu disable-dev-shm-usage],
+    binary: '/usr/bin/google-chrome'
+  )
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    options:
+  )
+end
+
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  DRIVER = if ENV["IS_CI"]
+    :headless_chrome
+  else
+    :chrome
+  end
+
+  driven_by :selenium, using: DRIVER, screen_size: [1400, 1400]
+end
