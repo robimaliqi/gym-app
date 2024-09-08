257f802a1a058539dfde94c0fd68d91a75ac8611
Robi <robi.maliqi@hotmail.co.uk>
2024-09-08 17:06:18 +0100

    Moved code from test helper to application system test case file

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,3 +3,29 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 end
+
+is_ci = ENV.fetch('IS_CI', false)
+
+Capybara.register_driver :headless_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(
+    args: %w[headless no-sandbox disable-gpu disable-dev-shm-usage],
+    binary: '/usr/bin/google-chrome'
+  )
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    options:
+  )
+end
+
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  DRIVER = if ENV["IS_CI"]
+    :headless_chrome
+  else
+    :chrome
+  end
+  puts DRIVER
+
+  driven_by :selenium, using: DRIVER, screen_size: [1400, 1400]
+end
